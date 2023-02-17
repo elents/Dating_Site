8477b2c52d50703d57be685841299dd28f5c2960
Elena Tsvetkova <elents@gmail.com>
2023-02-17 11:59:38 -0500

    Changes on slider

--- My Profile/slider/slider.js
+++ My Profile/slider/slider.js
@@ -1,7 +1,10 @@
-let offset = 0;
-const sliderLine = document.querySelector('.slider-line');
 
-document.querySelector('.slider-next').addEventListener('click', function(){
+
+
+    let offset = 0;
+    const sliderLine = document.querySelector('#slider-line_1');
+document.querySelector('#button_next_1').addEventListener('click', function(){
+    alert("hello");
     offset += 150;
     if(offset >= 600) {
         offset = 0;
@@ -9,4 +12,19 @@ document.querySelector('.slider-next').addEventListener('click', function(){
     sliderLine.style.left = -offset + 'px';
     console.log(offset);
     console.log(sliderLine.style.left);
+    document.querySelector('#button_next_2').click(); 
+});
+
+    let offset1 = 0;
+    const sliderLine1 = document.querySelector('#slider-line_2');
+document.querySelector('#button_next_2').addEventListener('click', function(){
+    alert("hello 2");
+    offset1 += 150;
+    console.log("22 ", offset1);
+    if(offset1 >= 600) {
+        offset1 = 0;
+    }
+    sliderLine1.style.left = -offset1 + 'px';
+    console.log(offset1);
+    console.log(sliderLine1.style.left);
 });
